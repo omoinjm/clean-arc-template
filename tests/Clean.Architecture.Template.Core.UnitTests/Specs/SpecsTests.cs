fd3491e54ce13b0ee1a3ef2397e2e0dab007a78a
omoinjm <njmcloud@gmail.com>
2026-01-12 15:30:21 +0200

    good for now

--- tests/Clean.Architecture.Template.Core.UnitTests/Specs/SpecsTests.cs
+++ tests/Clean.Architecture.Template.Core.UnitTests/Specs/SpecsTests.cs
@@ -5,80 +5,97 @@ namespace Clean.Architecture.Template.Core.UnitTests.Specs;
 public class PaginationTests
 {
     [Fact]
-    public void Pagination_WhenCreated_ShouldInitializeWithValidPageNumber()
+    public void Pagination_WhenCreated_ShouldInitializeWithValidPageIndex()
     {
         // Arrange
-        const int pageNumber = 1;
+        const int pageIndex = 1;
         const int pageSize = 10;
+        var data = new List<string> { "item1" };
 
         // Act
-        var pagination = new Pagination
-        {
-            PageNumber = pageNumber,
-            PageSize = pageSize
-        };
+        var pagination = new Pagination<string>(pageIndex, pageSize, 1, data);
 
         // Assert
-        pagination.PageNumber.Should().Be(pageNumber);
+        pagination.PageIndex.Should().Be(pageIndex);
         pagination.PageSize.Should().Be(pageSize);
+        pagination.Count.Should().Be(1);
+        pagination.Data.Should().HaveCount(1);
     }
 
     [Fact]
-    public void Pagination_WithZeroPageSize_ShouldSetPageSize()
+    public void Pagination_WithDefaultConstructor_ShouldInitializeEmptyData()
     {
         // Arrange & Act
-        var pagination = new Pagination
-        {
-            PageSize = 0
-        };
+        var pagination = new Pagination<string>();
 
         // Assert
-        pagination.PageSize.Should().Be(0);
+        pagination.Data.Should().BeEmpty();
     }
 
     [Theory]
     [InlineData(1, 10)]
     [InlineData(2, 20)]
     [InlineData(5, 50)]
-    public void Pagination_WithValidPageNumbers_ShouldSetCorrectly(int pageNumber, int pageSize)
+    public void Pagination_WithValidParameters_ShouldSetCorrectly(int pageIndex, int pageSize)
     {
-        // Arrange & Act
-        var pagination = new Pagination
-        {
-            PageNumber = pageNumber,
-            PageSize = pageSize
-        };
+        // Arrange
+        var data = new List<string>();
+
+        // Act
+        var pagination = new Pagination<string>(pageIndex, pageSize, 0, data);
 
         // Assert
-        pagination.PageNumber.Should().Be(pageNumber);
+        pagination.PageIndex.Should().Be(pageIndex);
         pagination.PageSize.Should().Be(pageSize);
+    }
+
+    [Fact]
+    public void Pagination_CanSetPropertiesAfterCreation()
+    {
+        // Arrange
+        var pagination = new Pagination<string>();
+        const int newPageIndex = 3;
+        const int newPageSize = 25;
+
+        // Act
+        pagination.PageIndex = newPageIndex;
+        pagination.PageSize = newPageSize;
+
+        // Assert
+        pagination.PageIndex.Should().Be(newPageIndex);
+        pagination.PageSize.Should().Be(newPageSize);
     }
 }
 
 public class GeneralSpecParamsTests
 {
     [Fact]
-    public void GeneralSpecParams_WhenCreated_ShouldInitializeProperties()
+    public void GeneralSpecParams_WhenCreated_ShouldInitializeWithDefaults()
     {
         // Arrange & Act
         var specs = new GeneralSpecParams();
 
         // Assert
         specs.Should().NotBeNull();
+        specs.PageIndex.Should().Be(1);
+        specs.PageSize.Should().Be(100);
+        specs.Sort.Should().Be("1");
+        specs.SortAscending.Should().BeTrue();
+        specs.Search.Should().Be("");
     }
 
     [Fact]
-    public void GeneralSpecParams_CanSetPageNumber()
+    public void GeneralSpecParams_CanSetPageIndex()
     {
         // Arrange
         var specs = new GeneralSpecParams();
-        const int pageNumber = 2;
+        const int pageIndex = 2;
 
         // Act
-        specs.PageNumber = pageNumber;
+        specs.PageIndex = pageIndex;
 
         // Assert
-        specs.PageNumber.Should().Be(pageNumber);
+        specs.PageIndex.Should().Be(pageIndex);
     }
 
     [Fact]
@@ -93,6 +110,20 @@ public class GeneralSpecParamsTests
 
         // Assert
         specs.PageSize.Should().Be(pageSize);
+    }
+
+    [Fact]
+    public void GeneralSpecParams_PageSizeExceedingMaximum_ShouldCapToMax()
+    {
+        // Arrange
+        var specs = new GeneralSpecParams();
+        const int maxPageSize = 1000;
+
+        // Act
+        specs.PageSize = 2000;
+
+        // Assert
+        specs.PageSize.Should().Be(maxPageSize);
     }
 
     [Fact]
@@ -124,28 +155,48 @@ public class GeneralSpecParamsTests
     }
 
     [Fact]
+    public void GeneralSpecParams_CanSetDates()
+    {
+        // Arrange
+        var specs = new GeneralSpecParams();
+        var fromDate = new DateTime(2024, 1, 1);
+        var toDate = new DateTime(2024, 12, 31);
+
+        // Act
+        specs.FromDate = fromDate;
+        specs.ToDate = toDate;
+
+        // Assert
+        specs.FromDate.Should().Be(fromDate);
+        specs.ToDate.Should().Be(toDate);
+    }
+
+    [Fact]
     public void GeneralSpecParams_WithAllProperties()
     {
         // Arrange
-        const int pageNumber = 3;
+        const int pageIndex = 3;
         const int pageSize = 50;
         const string search = "search term";
         const string sort = "createdDate";
+        var fromDate = new DateTime(2024, 1, 1);
 
         // Act
         var specs = new GeneralSpecParams
         {
-            PageNumber = pageNumber,
+            PageIndex = pageIndex,
             PageSize = pageSize,
             Search = search,
-            Sort = sort
+            Sort = sort,
+            FromDate = fromDate
         };
 
         // Assert
-        specs.PageNumber.Should().Be(pageNumber);
+        specs.PageIndex.Should().Be(pageIndex);
         specs.PageSize.Should().Be(pageSize);
         specs.Search.Should().Be(search);
         specs.Sort.Should().Be(sort);
+        specs.FromDate.Should().Be(fromDate);
     }
 }
 
@@ -162,45 +213,113 @@ public class LookupParamsTests
     }
 
     [Fact]
-    public void LookupParams_CanSetTableName()
+    public void LookupParams_CanSetLookupTableName()
     {
         // Arrange
         var lookupParams = new LookupParams();
         const string tableName = "Users";
 
         // Act
-        lookupParams.TableName = tableName;
+        lookupParams.LookupTableName = tableName;
 
         // Assert
-        lookupParams.TableName.Should().Be(tableName);
+        lookupParams.LookupTableName.Should().Be(tableName);
     }
 
     [Fact]
-    public void LookupParams_CanSetSearchField()
+    public void LookupParams_CanSetLookupPrimaryKey()
     {
         // Arrange
         var lookupParams = new LookupParams();
-        const string searchField = "Name";
+        const string primaryKey = "Id";
 
         // Act
-        lookupParams.SearchField = searchField;
+        lookupParams.LookupPrimaryKey = primaryKey;
 
         // Assert
-        lookupParams.SearchField.Should().Be(searchField);
+        lookupParams.LookupPrimaryKey.Should().Be(primaryKey);
     }
 
     [Fact]
-    public void LookupParams_CanSetSearchValue()
+    public void LookupParams_CanSetLookupName()
     {
         // Arrange
         var lookupParams = new LookupParams();
-        const string searchValue = "John";
+        const string lookupName = "UserName";
 
         // Act
-        lookupParams.SearchValue = searchValue;
+        lookupParams.LookupName = lookupName;
 
         // Assert
-        lookupParams.SearchValue.Should().Be(searchValue);
+        lookupParams.LookupName.Should().Be(lookupName);
+    }
+
+    [Fact]
+    public void LookupParams_CanSetLookupCode()
+    {
+        // Arrange
+        var lookupParams = new LookupParams();
+        const string lookupCode = "USR001";
+
+        // Act
+        lookupParams.LookupCode = lookupCode;
+
+        // Assert
+        lookupParams.LookupCode.Should().Be(lookupCode);
+    }
+
+    [Fact]
+    public void LookupParams_CanSetSelectedIds()
+    {
+        // Arrange
+        var lookupParams = new LookupParams();
+        const string selectedIds = "1,2,3";
+
+        // Act
+        lookupParams.SelectedIds = selectedIds;
+
+        // Assert
+        lookupParams.SelectedIds.Should().Be(selectedIds);
+    }
+
+    [Fact]
+    public void LookupParams_CanSetUseCustomQuery()
+    {
+        // Arrange
+        var lookupParams = new LookupParams();
+
+        // Act
+        lookupParams.UseCustomQuery = true;
+
+        // Assert
+        lookupParams.UseCustomQuery.Should().BeTrue();
+    }
+
+    [Fact]
+    public void LookupParams_CanSetExcludeId()
+    {
+        // Arrange
+        var lookupParams = new LookupParams();
+        const int excludeId = 5;
+
+        // Act
+        lookupParams.ExcludeId = excludeId;
+
+        // Assert
+        lookupParams.ExcludeId.Should().Be(excludeId);
+    }
+
+    [Fact]
+    public void LookupParams_CanSetIncludeNoneOption()
+    {
+        // Arrange
+        var lookupParams = new LookupParams();
+
+        // Act
+        lookupParams.IncludeNoneOption = true;
+
+        // Assert
+        lookupParams.IncludeNoneOption.Should().BeTrue();
     }
 
     [Fact]
@@ -208,21 +327,25 @@ public class LookupParamsTests
     {
         // Arrange
         const string tableName = "Roles";
-        const string searchField = "RoleName";
-        const string searchValue = "Admin";
+        const string primaryKey = "RoleId";
+        const string lookupName = "RoleName";
 
         // Act
         var lookupParams = new LookupParams
         {
-            TableName = tableName,
-            SearchField = searchField,
-            SearchValue = searchValue
+            LookupTableName = tableName,
+            LookupPrimaryKey = primaryKey,
+            LookupName = lookupName,
+            UseCustomQuery = true,
+            IncludeNoneOption = true
         };
 
         // Assert
-        lookupParams.TableName.Should().Be(tableName);
-        lookupParams.SearchField.Should().Be(searchField);
-        lookupParams.SearchValue.Should().Be(searchValue);
+        lookupParams.LookupTableName.Should().Be(tableName);
+        lookupParams.LookupPrimaryKey.Should().Be(primaryKey);
+        lookupParams.LookupName.Should().Be(lookupName);
+        lookupParams.UseCustomQuery.Should().BeTrue();
+        lookupParams.IncludeNoneOption.Should().BeTrue();
     }
 }
 
@@ -237,48 +360,21 @@ public class DataListTests
         // Assert
         dataList.Should().NotBeNull();
         dataList.Data.Should().BeEmpty();
+        dataList.Count.Should().Be(0);
     }
 
     [Fact]
-    public void DataList_CanSetPageNumber()
+    public void DataList_CanSetCount()
     {
         // Arrange
         var dataList = new DataList<string>();
-        const int pageNumber = 2;
+        const long count = 42;
 
         // Act
-        dataList.PageNumber = pageNumber;
+        dataList.Count = count;
 
         // Assert
-        dataList.PageNumber.Should().Be(pageNumber);
-    }
-
-    [Fact]
-    public void DataList_CanSetPageSize()
-    {
-        // Arrange
-        var dataList = new DataList<string>();
-        const int pageSize = 25;
-
-        // Act
-        dataList.PageSize = pageSize;
-
-        // Assert
-        dataList.PageSize.Should().Be(pageSize);
-    }
-
-    [Fact]
-    public void DataList_CanSetTotalRecords()
-    {
-        // Arrange
-        var dataList = new DataList<string>();
-        const int totalRecords = 100;
-
-        // Act
-        dataList.TotalRecords = totalRecords;
-
-        // Assert
-        dataList.TotalRecords.Should().Be(totalRecords);
+        dataList.Count.Should().Be(count);
     }
 
     [Fact]
@@ -297,27 +393,38 @@ public class DataListTests
     }
 
     [Fact]
-    public void DataList_CanSetAllProperties()
+    public void DataList_WithConstructorParameters()
     {
         // Arrange
-        const int pageNumber = 2;
-        const int pageSize = 20;
-        const int totalRecords = 100;
-        var data = new List<int> { 1, 2, 3, 4, 5 };
+        const int count = 100;
+        var data = new List<string> { "Item1", "Item2", "Item3", "Item4", "Item5" };
 
         // Act
-        var dataList = new DataList<int>
-        {
-            PageNumber = pageNumber,
-            PageSize = pageSize,
-            TotalRecords = totalRecords,
-            Data = data
-        };
+        var dataList = new DataList<string>(count, data);
 
         // Assert
-        dataList.PageNumber.Should().Be(pageNumber);
-        dataList.PageSize.Should().Be(pageSize);
-        dataList.TotalRecords.Should().Be(totalRecords);
+        dataList.Count.Should().Be(count);
         dataList.Data.Should().BeEquivalentTo(data);
+        dataList.Data.Should().HaveCount(5);
+    }
+
+    [Fact]
+    public void DataList_WithMultipleUpdates()
+    {
+        // Arrange
+        var dataList = new DataList<string>();
+        var firstData = new List<string> { "A", "B" };
+        var secondData = new List<string> { "X", "Y", "Z" };
+
+        // Act
+        dataList.Count = 2;
+        dataList.Data = firstData;
+        dataList.Count = 3;
+        dataList.Data = secondData;
+
+        // Assert
+        dataList.Count.Should().Be(3);
+        dataList.Data.Should().HaveCount(3);
+        dataList.Data.Should().BeEquivalentTo(secondData);
     }
 }
--- tests/TestCommon/TestConstants/Constants.cs
+++ tests/TestCommon/TestConstants/Constants.cs
@@ -12,12 +12,58 @@ public static class Constants
         public const string Surname = "Doe";
         public const string Email = "john.doe@example.com";
         public const string Username = "johndoe";
-        public const string Password = "password";
-        public const string Salt = "salt";
+        public const string Password = "Password123!";
+        public const string Salt = "salt-value-123";
         public const string IdNumber = "123456789";
         public const string PhoneNumber = "1234567890";
         public const string Role = "User";
-        public const string Token = "token";
+        public const string Token = "jwt-token-sample";
         public const bool IsActive = true;
+    }
+
+    public static class Menu
+    {
+        public const int ModuleId = 1;
+        public const int ModuleItemId = 1;
+        public const int SortOrder = 1;
+        public const int ModuleSortOrder = 1;
+        public const string DisplayText = "Dashboard";
+        public const string Icon = "dashboard-icon";
+        public const string ModuleDisplayText = "System";
+        public const string ModuleIcon = "system-icon";
+        public const string RouterLink = "/dashboard";
+        public const string ModuleRouterLink = "/system";
+        public const string ModuleSidebarClass = "menu-primary";
+    }
+
+    public static class Auth
+    {
+        public const string LoginEmail = "login@example.com";
+        public const string LoginPassword = "LoginPassword123!";
+        public const string RegisterEmail = "register@example.com";
+        public const string RegisterPassword = "RegisterPassword123!";
+        public const string RegisterFirstName = "FirstName";
+        public const string RegisterLastName = "LastName";
+    }
+
+    public static class Pagination
+    {
+        public const int DefaultPageNumber = 1;
+        public const int DefaultPageSize = 10;
+        public const int MaxPageSize = 100;
+        public const int TotalRecords = 50;
+    }
+
+    public static class Lookup
+    {
+        public const string TableName = "UserStatuses";
+        public const string SearchField = "Name";
+        public const string SearchValue = "Active";
+    }
+
+    public static class Query
+    {
+        public const string SearchTerm = "test";
+        public const string SortField = "Name";
     }
 }
